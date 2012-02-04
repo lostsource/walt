04e29ebf95fa1d7b3f3c0f58bc627aaf272cb572
Sven Jacobs <mail@svenjacobs.com>
2012-02-05 00:49:24 +0100

    Working on config loading.

--- plugins/javascript.js
+++ plugins/javascript.js
@@ -1,12 +1,10 @@
-/*jslint white: true, nomen: true, regexp: true, indent: 4*/
-/*globals require, console, exports*/
-(function() {
+/*jshint browser: true, node: true, regexp: false, indent: 4*/
+(function () {
     'use strict';
 
     var qs = require('querystring'),
         http = require('http'),
-        common = require('../common.js'),
-        lint = require('./javascript/jslint.js');
+        common = require('../common.js');
 
     /**
      * JavaScript plugin for BuildJS
@@ -35,9 +33,9 @@
      * cancelled.
      *
      * @param data The file's contents
-     * @param callback Is a function(data)
+     * @param callback Is a function (data)
      */
-    BuildJS_JavaScript.prototype.onFile = function(data, callback) {
+    BuildJS_JavaScript.prototype.onFile = function (data, callback) {
         // Make sure we strip a shebang for validation/compilation
         // and prepend it again later
         var self = this,
@@ -49,7 +47,7 @@
             data = data.replace(this.SHEBANG_REGEX, '');
         }
         
-        this.closure(data, function(result) {
+        this.closure(data, function (result) {
             callback({
                 data: shebang ? shebang + '\n' + result.compiledCode : result.compiledCode,
                 errors: self.mixinToString(result.serverErrors, self.serverErrorsToString) || self.mixinToString(result.errors, self.errorsToString) || null,
@@ -61,7 +59,7 @@
     /**
      * @private
      */
-    BuildJS_JavaScript.prototype.mixinToString = function(obj, func) {
+    BuildJS_JavaScript.prototype.mixinToString = function (obj, func) {
         if (obj === undefined) {
             return undefined;
         }
@@ -73,10 +71,10 @@
     /**
      * @private
      */
-    BuildJS_JavaScript.prototype.serverErrorsToString = function() {
+    BuildJS_JavaScript.prototype.serverErrorsToString = function () {
         var out = '';
         
-        this.forEach(function(error) {
+        this.forEach(function (error) {
             out += 'Code: ' + error.code + ', Error: ' + error.error + '\n';
         });
 
@@ -86,10 +84,10 @@
     /**
      * @private
      */
-    BuildJS_JavaScript.prototype.errorsToString = function() {
+    BuildJS_JavaScript.prototype.errorsToString = function () {
         var out = '';
         
-        this.forEach(function(error) {
+        this.forEach(function (error) {
             out += 'Line: ' + error.lineno + ', Char: ' + error.charno + ', Error: ' + error.error + '\n';
         });
 
@@ -99,10 +97,10 @@
     /**
      * @private
      */
-    BuildJS_JavaScript.prototype.warningsToString = function() {
+    BuildJS_JavaScript.prototype.warningsToString = function () {
         var out = '';
         
-        this.forEach(function(warning) {
+        this.forEach(function (warning) {
             out += 'Line: ' + warning.lineno + ', Char: ' + warning.charno + ', Warning: ' + warning.warning + '\n';
         });
 
@@ -115,41 +113,22 @@
     BuildJS_JavaScript.prototype.e = common.e;
 
     /**
-     * @private
-     */
-    /*BuildJS_JavaScript.prototype.javascript = function(file) {
-        console.log('=== Running JSLint on ' + file + ' ===\n');
-        var content = fs.readFileSync(file, 'utf-8'),
-            result = lint.JSLINT(content);
-        //overall &= result;
-        if (!result) {
-            lint.JSLINT.errors.forEach(function(err) {
-                console.log('    line: ' + err.line);
-                console.log('    character: ' + err.character);
-                console.log('    reason: ' + err.reason + '\n');
-            });
-        } else {
-            console.log('    OK\n');
-        }
-    };*/
-
-    /**
      * Applys Google Closure Compiler on JavaScript file.
      *
-     * @param callback function(closureResponse)
+     * @param callback function (closureResponse)
      *                 closureResponse is an object, see http://code.google.com/closure/compiler/docs/api-ref.html
      *
      * @private
      */
-    BuildJS_JavaScript.prototype.closure = function(data, callback) {
+    BuildJS_JavaScript.prototype.closure = function (data, callback) {
         var body = qs.stringify({
                 js_code: data,
                 compilation_level: 'SIMPLE_OPTIMIZATIONS',
                 output_format: 'json'
-            }) + '&output_info=compiled_code'
-               + '&output_info=warnings'
-               + '&output_info=errors'
-               + '&output_info=statistics',
+            }) + '&output_info=compiled_code' +
+                 '&output_info=warnings' +
+                 '&output_info=errors' +
+                 '&output_info=statistics',
 
             req = http.request({
                 host: 'closure-compiler.appspot.com',
@@ -159,23 +138,23 @@
                     'Content-Length': body.length,
                     'Content-Type': 'application/x-www-form-urlencoded'
                 }
-            }, function(res) {
+            }, function (res) {
                 var result = '';
 
                 res.setEncoding('utf8');
 
-                res.on('data', function(chunk) {
+                res.on('data', function (chunk) {
                     result += chunk;
                 });
 
-                res.on('end', function() {
+                res.on('end', function () {
                     var json = JSON.parse(result);
                     callback(json);
                 });
             }
         );
 
-        req.on('error', function(err) {
+        req.on('error', function (err) {
             console.error(err);
         });
 
@@ -186,7 +165,7 @@
         name: 'JavaScript',
         version: '0.0.1',
         fileTypes: ['js'],
-        provider: function() {
+        provider: function () {
             return new BuildJS_JavaScript();
         }
     };
